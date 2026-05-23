7ecce78a69af04ce4948602ffc10a0c6dc5e706e
Rohith Achar <rohithachar2000@gmail.com>
2026-05-23 19:08:07 +0530

    clean slate

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useRef, useState } from "react";
-import gsap from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+import gsap from "gsap";
 
 gsap.registerPlugin(ScrollTrigger);
 
@@ -11,12 +12,6 @@ export const Hero = () => {
   const rightTreesRef = useRef(null);
   const mainTextRef = useRef(null);
   const kidsRef = useRef(null);
-  const blackOverlayRef = useRef(null);
-  const abhyudayWrapperRef = useRef(null);
-  const abhyudayBgRef = useRef(null);
-  const abhyudayImageRef = useRef(null);
-  const abhyudayTenRef = useRef(null);
-  const dateRef = useRef(null);
 
   const [isMobile, setIsMobile] = useState(window.innerWidth <= 768);
 
@@ -79,7 +74,7 @@ export const Hero = () => {
     });
 
     gsap.to(mainTextRef.current, {
-      y: -50,
+      y: -150,
       opacity: 0,
       ease: "power3.out",
       scrollTrigger: {
@@ -102,97 +97,6 @@ export const Hero = () => {
       },
     });
 
-    gsap.to(blackOverlayRef.current, {
-      opacity: 1,
-      ease: "power3.out",
-      scrollTrigger: {
-        trigger: container,
-        start: 600,
-        end: 900,
-        scrub: true,
-      },
-    });
-
-    gsap.set(abhyudayWrapperRef.current, {
-      scale: isMobile ? 80 : 150,
-      xPercent: isMobile ? -190 : -270,
-      yPercent: -50,
-    });
-    gsap.set(abhyudayBgRef.current, {
-      opacity: 0,
-    });
-
-    gsap.to(abhyudayWrapperRef.current, {
-      scale: 1,
-      xPercent: -50,
-      ease: "power3.out",
-      scrollTrigger: {
-        trigger: container,
-        start: 300,
-        end: 1500,
-        scrub: 1,
-      },
-    });
-    gsap.to(abhyudayBgRef.current, {
-      opacity: 1,
-      ease: "power3.out",
-      scrollTrigger: {
-        trigger: container,
-        start: 600,
-        end: 1400,
-        scrub: 1,
-      },
-    });
-
-    gsap.set(abhyudayTenRef.current, {
-      xPercent: -50,
-      y: isMobile ? -35 : -80, // move up
-      left: "50%",
-      top: "100%",
-      opacity: 0,
-      scale: 1.1,
-    });
-    gsap.set(dateRef.current, {
-      xPercent: -50,
-      opacity: 0,
-      y: 140, // move up
-    });
-
-    gsap.to([abhyudayTenRef.current, dateRef.current], {
-      opacity: 1,
-      scale: 1,
-      stagger: 1,
-      ease: "power3.out",
-      scrollTrigger: {
-        trigger: container,
-        start: 1500,
-        end: 2000,
-        scrub: true,
-      },
-    });
-    gsap.to(abhyudayTenRef.current, {
-      color: "red", // move down to original position
-      ease: "power3.out",
-      scrollTrigger: {
-        trigger: container,
-        start: 1500,
-        end: 2000,
-        scrub: true,
-      },
-    });
-
-    gsap.to(abhyudayWrapperRef.current, {
-      opacity: 0,
-      y: 10,
-      ease: "power3.out",
-      scrollTrigger: {
-        trigger: container,
-        start: 2300,
-        end: 3000,
-        scrub: true,
-      },
-    });
-
     return () => {
       ScrollTrigger.getAll().forEach((trigger) => trigger.kill());
     };
@@ -211,10 +115,6 @@ export const Hero = () => {
           overflow: "hidden",
         }}
       >
-        <div
-          ref={blackOverlayRef}
-          className="w-screen h-screen absolute top-0 left-0 bg-black z-20 opacity-0"
-        />
         {/* Background image */}
         <img
           ref={heroImageRef}
@@ -227,6 +127,10 @@ export const Hero = () => {
             height: "100%",
             objectFit: "cover",
             zIndex: 1,
+            WebkitMaskImage:
+              "linear-gradient(to bottom, black 55%, transparent 100%)",
+            maskImage:
+              "linear-gradient(to bottom, black 55%, transparent 100%)",
           }}
         />
 
@@ -301,49 +205,9 @@ export const Hero = () => {
             zIndex: 5,
           }}
         />
-
-        {/* Abhyuday wrapper */}
-        <div
-          ref={abhyudayWrapperRef}
-          className="absolute left-1/2 top-1/3 -translate-x-1/2 -translate-y-1/2 inline-block"
-          style={{
-            zIndex: 21,
-          }}
-        >
-          {/* White background */}
-          <div
-            ref={abhyudayBgRef}
-            className="absolute inset-0 bg-white mx-auto my-auto"
-            style={{
-              zIndex: 1,
-              borderRadius: "8px",
-              width: "99%",
-              height: "99%",
-            }}
-          />
-
-          {/* SVG */}
-          <img
-            ref={abhyudayImageRef}
-            src="/abhyuday_new.svg"
-            alt="Abhyuday"
-            className="relative z-[5] w-auto h-auto max-w-[70vw]"
-          />
-
-          <h1
-            ref={abhyudayTenRef}
-            className="z-[6] absolute left-1/2 text-white text-[150px] leading-none font-bold"
-          >
-            10.0
-          </h1>
-          <p
-            ref={dateRef}
-            className="z-[6] absolute left-1/2 leading-none text-white"
-          >
-            COMING SOON
-          </p>
-        </div>
       </div>
+
+      <div className="absolute"></div>
 
       <div style={{ height: "1px" }} />
     </div>
